2e0aeea4a64181d6d2645b081077a7be0e86347b
Ayah Hussein <joudzyoud1992@gmail.com>
2021-02-09 12:07:22 +0200

    edit

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
+console.log('class 04');
 
+var name = prompt ("type your name please :");
+var ask = prompt ("what is the best italian dish for you?");
+var choose = prompt ("choose food");
 
+console.log('your name is:' + name);
+console.log('your best italian dish is:' + ask);
+document.write('<p>your name:'+ name +',' + 'your favorite dish'+ ask +  '</p>' );
 
-
-
-
-
-
-
-
-
-var order= prompt ("do you want pizza or Pasta?");
-
-IF (order == "pasta") {
-
-    alert ("this is not availabe now");
-}
-else
-
+if (choose == false) 
 {
+   console.log ("you don't select a food");
+//    document.write("<h1> you don't select a food</h1>");
 
-    alert ("welcome to our resturant");
+} 
+else
+{
+     console.log('the price for this dish is 20$');
+     document.write("the price for this dish is 20$");
+
 }
-
-
-
-
